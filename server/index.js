1c1400736706b7b58d042b563ffdbf0de0760c09
Green Hacker <hhirawat5@gmail.com>
2025-10-31 13:30:12 +0530

    chore: initialize server package.json with dependencies and scripts

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', function (req, res) {
+  res.send({ hello: 'world' })
+})
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
